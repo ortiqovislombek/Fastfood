acda64fc76dc47d3b3d2f2a10fb9fa3d69bda338
Islombek <154363858+ortiqovislombek@users.noreply.github.com>
2025-09-19 12:55:12 +0500

    Update main.py
    update

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-from aiogram import Bot,Dispatcher,F
+from aiogram import Bot,Dispatcher
 from environs import Env
 import asyncio
 import logging
-from hendler import admin_router,user_router
-dp=Dispatcher()
-env=Env()
 
-env.read_env()
+from hendler import admin_router,user_router
+import os
+
+os.makedirs("images",exist_ok=True)
+
+dp = Dispatcher()
+
+
+
 
 async def main():
-    TOKEN=env.str("TOKEN")
-    bot=Bot(TOKEN)
-    dp.include_router(user_router)
+   
+    TOKEN = os.getenv("TOKEN")
+    bot = Bot(TOKEN)
     dp.include_router(admin_router)
+    dp.include_router(user_router)
     await dp.start_polling(bot)
-if __name__=="__main__":
-        logging.basicConfig(level=logging.INFO)
-        try:
-            asyncio.run(main())
-        except Exception as err:
-            print(err)    
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+        asyncio.run(main())
+    except Exception as err:
+        print(err)
